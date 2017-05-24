41bcda06dbf881e7ca86407f2257ca1e9a68f193
Yoshi <Yoshi@AZUSA>
2017-05-24 16:23:13 +0200

    Projektdateien hinzufügen.

---
+++ DupeHandling.View/FileOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace DupeHandling
+{
+    class FileOperations
+    {
+        public string path;
+        public string fileName;
+
+        public void DeleteFileToBin(string file)
+        {
+            FileSystem.DeleteFile(file, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+        }
+
+        public void DeleteFile(string file)
+        {
+            File.Delete(file);
+
+        }
+    }
+}
